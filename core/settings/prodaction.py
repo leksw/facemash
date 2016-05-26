4f52613993b9480cbecde25dc6ed1781ae486195
Алексей <aleks.woronow@yandex.ru>
2016-05-26 20:51:26 +0000

    add db settings

--- core/settings/prodaction.py
+++ core/settings/prodaction.py
@@ -2,22 +2,34 @@
 from __future__ import unicode_literals
 
 from .base import *
-from django.core.exceptions import ImproperlyConfigured
-
-
-def get_env_variable(var_name):
-    try:
-        return os.environ[var_name]
-    except KeyError:
-        error_msg = "Set the %s environment variable" % var_name
-        raise ImproperlyConfigured(error_msg)
- 
-SECRET_KEY = get_env_variable('SECRET_KEY')
 
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['aleks.pythonanywhere.com']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'aleks$facemash',
+        'USER': 'aleks',
+        'PASSWORD': 'facemash1',
+        "HOST": 'aleks.mysql.pythonanywhere-services.com'
+    }
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
+        'BINARY': True
+    }
+}
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'files', 'media')
+
+
+os.environ['MEMCACHE_SERVERS'] = 'pub-memcache-10305.us-east-1-2.4.ec2.garantiadata.com:10305' 
+os.environ['MEMCACHE_USERNAME'] = 'aleks'
+os.environ['MEMCACHE_PASSWORD'] = 'hjlbjy12'
