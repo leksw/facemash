f13fe6d21ad6521f0257469bc3343201f54e880e
Алексей <aleks.woronow@yandex.ru>
2016-05-26 19:28:01 +0000

    add sekret_key for settings

--- core/settings/prodaction.py
+++ core/settings/prodaction.py
@@ -2,6 +2,22 @@
 from __future__ import unicode_literals
 
 from .base import *
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+ 
+SECRET_KEY = get_env_variable('SECRET_KEY')
+
+
+DEBUG = False
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'files', 'media')
